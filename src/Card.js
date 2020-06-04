740515b2a9ee7cabca1b2419dd3a53be21f04657
anish391 <argads@gmail.com>
2020-06-04 00:12:27 +0000

    Init memory game react app.

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import './Card.css'
+
+const Card = (props) => {
+	return (
+		<div />
+	)
+}
+
+Card.propTypes = {
+	
+}
+
+export default Card;
